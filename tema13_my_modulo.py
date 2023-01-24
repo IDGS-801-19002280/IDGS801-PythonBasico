c5b7549bcb264fce12d48ccf9110efc5081d9a7c
Jesús Méndez IDGS-801-19002280 <19002280@alumnos.utleon.edu.mx>
2023-01-23 19:42:59 -0600

    Actividad 2 subida

---
+++ tema13_my_modulo.py
@@ -0,0 +1,13 @@
+def tem():
+    print("Hola desde my_modulo.py!")
+    
+def main():
+    print("Hola desde main!")
+    tem()
+    tem2()
+
+def tem2():
+    print("Hola desde my_modulo.py!")
+
+if __name__== '__main__':
+    main()
